03c2daab4b7d28ab90269b4948ad2f9aabcb6e2c
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-06-15 17:08:56 +0000

    ✅ Done

---
+++ 01 Output & Variable/ans_03.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Size of int  " << sizeof(int) << endl ;
+    cout << "Size of bool  " << sizeof(bool) << endl ;
+    cout << "Size of float  " << sizeof(float) << endl ;
+    return 0;
+}
