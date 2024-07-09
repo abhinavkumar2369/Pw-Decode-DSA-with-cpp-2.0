0b80326d82d6203f9722f804482b69b680b9906f
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-07-09 11:41:54 +0000

    ✅ Done

---
+++ 02 Input & Operators/ans_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"Enter first number\n";
+    cin>>x;
+    int y;
+    cout<<"Enter second number\n";
+    cin>>y;
+    cout<<(x!=y)<<" "<<(x>=y);
+}
+
+//  INPUT   x=3  y=2
+//  OUTPUT   1   1
