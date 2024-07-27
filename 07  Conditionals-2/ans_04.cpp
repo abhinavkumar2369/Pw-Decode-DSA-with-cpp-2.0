cde08a1f790f3d85091aabd67aaadb150267963b
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-07-27 10:23:49 +0000

    ✅ Done

---
+++ 07  Conditionals-2/ans_04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int test = 0;
+    cout << "First character " << '1' << endl;
+    cout << "Second character " << (test ? 3 : '1') << endl;
+    return 0;
+}
+
+//  OUTPUT ---
+//  First character 1
+//  Second character 49
