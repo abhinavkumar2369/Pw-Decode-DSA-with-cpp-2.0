73d6c3ff662b1b6113614605fff3a6d32cee10c6
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-07-27 10:22:09 +0000

    ✅ Done

---
+++ 02 Input & Operators/ans_05.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c1,c2;
+    cout << "Enter Character 1 = ";
+    cin >> c1;
+    cout << "Enter Character 2 = ";
+    cin >> c2;
+    cout << "ASCII value differnce is " << int(c1)-int(c2) ;
+    return 0;
+}
+
+//  INPUT  --   ch1='a'  ch2='A'
+//  OUTPUT --   32
