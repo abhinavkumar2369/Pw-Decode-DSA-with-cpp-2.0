bc416c181979a4f8e2232e90a75e04a2c7b28a4f
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-07-27 10:23:42 +0000

    ✅ Done

---
+++ 07  Conditionals-2/ans_03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 3 ;
+    float y = 3.0 ;
+    if (x == y)
+        cout <<"x and y are equal" ;
+    else
+        cout << "x and y are not equal" ;
+    return 0;
+}
+
+//  OUTPUT ---
+//  x and y are equal
