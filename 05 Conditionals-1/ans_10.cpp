6ba9e511e4a4710360bdfd9cc7e428fedbc2bbe1
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-16 14:08:44 +0000

    ✅ Done

---
+++ 05 Conditionals-1/ans_10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 500, b, c ;
+    if ( a >= 400 ) 
+        b = 300 ; 
+        c = 200 ; 
+        cout << "value of b and c are respectively " << b << " and " << c  ; 
+
+	return 0;
+}
+
+//  OUTPUT ---
+//  Value of b and c are respectively 300 and 200
+
