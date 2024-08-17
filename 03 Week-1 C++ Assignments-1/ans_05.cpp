b4328255657ae06d8f0358c687aa66cee7f8dfdc
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-17 14:24:31 +0000

    ✅ Done

---
+++ 03 Week-1 C++ Assignments-1/ans_05.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int radius=10;
+    float circumference ;
+    circumference = 2*M_PI*radius;
+    cout << circumference ;    
+}
