a4d37802e51151a0dd0287189f2358d5a53692bb
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-18 13:13:12 +0000

    ✅ Done

---
+++ 07  Conditionals-2/ans_06.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = -4;
+    char result;
+    result = number > 0 ? 'P' : 'N';
+    cout << result << endl;
+    return 0;
+}
+
+//  OUTPUT ---
+//  N
