02b01144b7681e0a450df0428e59f1e574e7beef
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-19 09:49:40 +0000

    ✅ Done

---
+++ 08 Week-2 C++ Conditionals-2/ans_05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 18; int b = 12;
+    bool t = (a > 20 && b < 15)? true : false;
+    cout <<"Value of t: " << t ;
+    return 0;
+}
+
+//  OUTPUT ---
+//  Value of t: 0
