4a8234c1b1e69d02c209da881cce8578de8250cd
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-08-20 08:34:25 +0000

    ✅ Done

---
+++ 15 Week-5 Array-1/ans_06.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sub[50], i ;
+    for ( i=0 ; i<=48 ; i++ );
+    {
+    sub[i]=i ;
+    cout<<sub[i]<<endl ;
+    }
+    return 0;
+}
+
+// Output ----
+// 49
