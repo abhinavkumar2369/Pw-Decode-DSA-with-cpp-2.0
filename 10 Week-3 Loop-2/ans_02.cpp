f7d11015c96123359353a351ad41ae8e7c2d7d50
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-22 03:37:48 +0530

    ✅ Done

---
+++ 10 Week-3 Loop-2/ans_02.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main( ) {
+    int t = 10;
+    while (t /= 2) {
+        cout << "Hello" << endl;
+    }
+}
+
+//  OUTPUT --
+
+//  Hello
+//  Hello
+//  Hello
