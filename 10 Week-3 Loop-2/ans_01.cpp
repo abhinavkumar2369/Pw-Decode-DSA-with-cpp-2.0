3ec5814a9407a7f396ed4012fdb57ad42e103f2f
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-22 03:37:42 +0530

    ✅ Done

---
+++ 10 Week-3 Loop-2/ans_01.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    while ('1' < '2')
+        cout << "In while loop" << endl;
+}
+
+//  OUTPUT --
+
+//  "In while loop" for infinite times
+//  Character '1' & '2' will be compared on the basis of their ASCII values.
+//  ASCII value for (1 is 49) & (2 in 50)
