287fdf59c1f9573b78c257c28fdcb18bd8853741
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-22 03:37:55 +0530

    ✅ Done

---
+++ 10 Week-3 Loop-2/ans_03.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int x = 1; x * x <= 10; x++)
+        cout << "In for loop" << endl;
+}
+
+
+//  OUTPUT --
+
+// In for loop
+// In for loop
+// In for loop
