1ea2e6e3afd2a1aca7a0e4f87aeff96dfc8021fb
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-23 03:38:33 +0530

    ✅ Done

---
+++ 10 Week-3 Loop-2/ans_04.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main( ) {
+    int x = 10, y = 0 ;
+    while ( x >= y ) {
+        x-- ;
+        y++ ;
+        cout << x << " " << y << endl ;
+    }
+    return 0;
+}
+
+// OUTPUT --
+
+// 9 1
+// 8 2
+// 7 3
+// 6 4
+// 5 5
+// 4 6
