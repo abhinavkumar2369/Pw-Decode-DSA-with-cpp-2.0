8c8fc0536a01d9b86cfa349ec527a27e30278b8f
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-24 03:54:24 +0530

    ✅ Done

---
+++ 11 Week-3 Printing Pattern-1/ans_01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i=1 ; i<=4 ; i++){
+        for (int j=1 ; j<=4 ; j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// OUTPUT :
+
+// 1111
+// 2222
+// 3333
+// 4444
