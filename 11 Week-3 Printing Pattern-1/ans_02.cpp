a159c50c4ca7c0f2f165b55312ca5829c0429e51
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-24 03:54:36 +0530

    ✅ Done

---
+++ 11 Week-3 Printing Pattern-1/ans_02.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    
+    cout << "Enter value of n = ";
+    cin >> n;
+
+    for (int i=0 ; i<n ; i++){
+        for (int j=1 ; j<=(n-i) ; j++){
+            cout << j ;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// INPUT :
+// Enter value of n = 4
+
+
+// OUTPUT :
+
+// 1234
+// 123
+// 12
+// 1
