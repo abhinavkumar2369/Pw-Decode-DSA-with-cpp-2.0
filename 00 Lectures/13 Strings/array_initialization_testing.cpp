98bd4f221bd7a99b95773991469d85a6d507bdec
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-11-08 20:44:02 +0530

    📒 Noted

---
+++ 00 Lectures/13 Strings/array_initialization_testing.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    char str[n];
+
+    for (int i=0; i<n; i++)  cin >> str[i];
+    cout << str << endl;
+
+    return 0;
+}
+
+
+// output
+
