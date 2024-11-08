60270e1c794e2f1e51d987eb4e440084dafb3034
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-11-08 20:43:57 +0530

    📒 Noted

---
+++ 00 Lectures/13 Strings/array_index_value_pre_incrementation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3};
+    cout << arr[1] << endl;
+    arr[1]++;
+    cout << arr[1] << endl;
+}
+
+// Answer ---
+// 2
+// 3
