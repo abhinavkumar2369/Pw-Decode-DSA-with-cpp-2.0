6f02b116065064e637051cb4679d310802f8b98d
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-11-08 20:44:08 +0530

    📒 Noted

---
+++ 00 Lectures/13 Strings/basics.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char str1[] = "Hello";
+    char str2[] = {'W','o','r','l','d'};
+
+    cout << str1 << endl;
+    cout << str2 << endl;
+    
+    return 0;
+}
+
+// ANSWER ---
+// Hello
+// WorldHello
