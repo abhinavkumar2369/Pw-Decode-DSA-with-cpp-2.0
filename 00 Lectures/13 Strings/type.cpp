5fd33b20c62b45b61bab6b0256ef0b3505332bf5
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-11-11 20:46:41 +0530

    📒 Noted

---
+++ 00 Lectures/13 Strings/type.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char a = 'a';
+    char b = '3';
+    cout << (int)a << endl;
+    cout << (int)b << endl;
+    // cout << stoi(b) << endl; will not work
+
+    return 0;
+}
