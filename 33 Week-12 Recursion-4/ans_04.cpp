26607b24123c7e8a811083e6b31c72383da8ffe3
Abhinav Kumar <abhinavkumar2369@outlook.com>
2025-01-24 02:56:56 +0530

    ✅ Done

---
+++ 33 Week-12 Recursion-4/ans_04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    }
+
+    return 0;
+}
