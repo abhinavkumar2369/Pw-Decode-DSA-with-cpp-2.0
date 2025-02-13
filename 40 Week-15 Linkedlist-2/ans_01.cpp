a7b5221a691752e17cf6d21929520902d6a1d2e1
Abhinav Kumar <abhinavkumar2369@outlook.com>
2025-02-14 03:03:11 +0530

    ✅ Done

---
+++ 40 Week-15 Linkedlist-2/ans_01.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* deleteMiddle(ListNode* head) {
+    if (!head || !head->next) return nullptr;
+    ListNode *slow = head, *fast = head, *prev = nullptr;
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    delete slow;
+    return head;
+}
+
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head = deleteMiddle(head);
+    printList(head);
+    
+    while (head) {
+        ListNode* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+    return 0;
+}
